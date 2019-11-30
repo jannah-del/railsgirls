67991a375b303c0c8764914f1a895e11f18a263c
jannah <jannah.hossain@gmail.com>
2019-11-30 16:41:55 +0000

    auth

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,6 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
-   include CarrierWave::MiniMagickem
+   include CarrierWave::MiniMagick
    version :thumb do
   process :resize_to_fit => [50, 50]
 end
